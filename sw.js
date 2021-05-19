786a6874938064971abd2c122931f4c9e35c0079
Hari Krishnan <75353186+Harikishnan@users.noreply.github.com>
2021-05-19 13:30:16 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,8 +2,8 @@ var APP_PREFIX = 'Rss'     // Identifier for this app (this needs to be consiste
 var VERSION = 'version_01'              // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION
 var URLS = [                            // Add URL you want to cache in this list.
-  '/{repository}/',                     // If you have separate JS/CSS files,
-  '/{repository}/index.html'            // add path to those files here
+  '/RssTvndrApp/',                     // If you have separate JS/CSS files,
+  '/RssTvndrApp/index.html'            // add path to those files here
 ]
 
 // Respond with cached resources
